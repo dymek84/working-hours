6f97a82b07a992f7011616c9ccde3785db0f7251
Dymek <dymek1984@gmail.com>
2022-04-10 16:19:18 +0200

    --

--- Cdate.php
+++ Cdate.php
@@ -37,12 +37,47 @@ class Cdate
     }
     public function isSunday()
     {
-        if (date("l", strtotime($this->date)) == "Saturday") {
+        if (date("l", strtotime($this->date)) == "Sunday") {
             return true;
         } else {
             return false;
         }
     }
-}
+    public function getWeekDayName()
+    {
+        return date("l", strtotime($this->date));
+    }
 
-$newDate = new Cdate("", "", "");
+    public function getDuration()
+    {
+        return (strtotime($this->finishHour) - strtotime($this->startHour)) / 60 / 60;
+    }
+    public function getBreaks()
+    {
+        return calculateBreaks($this);
+    }
+
+    public function getDurationWithoutBreaks()
+    {
+        return $this->getDuration() - $this->getBreaks();
+    }
+}
+$test = new Cdate("2022-04-02", "07:00:00", "16:30:00");
+echo $test->getDurationWithoutBreaks();
+
+function calculateBreaks($Cdate)
+{
+    $startTime = ($Cdate->getStart() == "09:30:00" ? "10:00:00" : $Cdate->getStart() == "12:30:00") ? "13:00:00" : $Cdate->getStart();
+    $stopTime = $Cdate->getFinish();
+    $firstBreak = "09:30:00";
+    $secondBreak = "12:30:00";
+    if ($startTime <= $firstBreak && $stopTime <= $firstBreak || $startTime >= $secondBreak && $stopTime >= $secondBreak || $startTime >= $firstBreak && $stopTime <= $secondBreak) {
+        return 0;
+    } elseif (($startTime <= $firstBreak && $stopTime >= $firstBreak && $stopTime <= $secondBreak) || ($startTime >= $firstBreak && $startTime <= $secondBreak && $stopTime >= $secondBreak)) {
+        return 0.5;
+    } elseif ($startTime <= $firstBreak && $stopTime >= $secondBreak) {
+        return 1;
+    } else {
+        return 999;
+    }
+}
